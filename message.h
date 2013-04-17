39dcefcb8e4714bd3c8b1cfc1a06936a557e268e
Johan Källström <johka546@student.liu.se>
2013-04-17 22:47:14 +0200

    Macros för diverser. Samt utils för gemensamma funktioner som kan vara bra att ha.

--- message.h
+++ message.h
@@ -52,18 +52,6 @@
 #define START 1 
 #define END 0 
 
-//macros
-#define BIT(x) (1 << (x))
-#define SETBITS(x,y) ((x) |= (y))
-#define CLEARBITS(x,y) ((x) &= (~(y)))
-#define SETBIT(x,y) SETBITS((x), (BIT((y))))
-#define CLEARBIT(x,y) CLEARBITS((x), (BIT((y))))
-#define BITSET(x,y) ((x) & (BIT(y)))
-#define BITCLEAR(x,y) !BITSET((x), (y))
-#define BITSSET(x,y) (((x) & (y)) == (y))
-#define BITSCLEAR(x,y) (((x) & (y)) == 0)
-#define BITVAL(x,y) (((x)>>(y)) & 1)
-
 //macros to code/decode messages
 #define GET_LEN(x) (x)&0b00011111
 #define SET_LEN(x) (x)
