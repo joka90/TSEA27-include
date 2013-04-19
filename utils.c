e5507b5a85525031524ecb25d726e2976715d7c3
Johan Kllstrm <johka546@student.liu.se>
2013-04-19 18:48:52 +0200

    Fixat filter.

--- utils.c
+++ utils.c
@@ -1,11 +1,13 @@
 #include "utils.h"
 
+
+//turn off optimization 
+#pragma GCC push_options
+#pragma GCC optimize ("O0")
 uint16_t filterSampleArrayMeanPlusPlus(volatile uint16_t  *samples, uint8_t numOfSamples, uint8_t threshold)
 {
-	uint8_t maxNumInRow=0;
 	uint8_t currentNumInRow=0;
 	uint16_t currentSum=0;
-	uint16_t bestSum=0;
 	for(uint8_t i = 0; i < numOfSamples-1; i++)
 	{
 		if(absThreshold(samples[i], samples[i+1]) < threshold)
@@ -13,30 +15,24 @@ uint16_t filterSampleArrayMeanPlusPlus(volatile uint16_t  *samples, uint8_t numO
 			currentNumInRow++;
 			currentSum+=samples[i];//summera
 		}
-		else
-		{
-			if(maxNumInRow<=currentNumInRow)
-			{
-				bestSum=currentSum+samples[i];
-				maxNumInRow=currentNumInRow;
-			}
-			currentNumInRow=0;
-		}
-		if(maxNumInRow<=currentNumInRow)
-		{
-			if(!(i < numOfSamples-2))//sista
-			{
-				bestSum=currentSum+samples[i+1];
-			}
-			else
-			{
-				bestSum=currentSum;
-			}
-			maxNumInRow=currentNumInRow;
-		}
 	}
-	return (uint16_t)(bestSum/(maxNumInRow+1));//return result
+	if(absThreshold(samples[0], samples[numOfSamples-1]) < threshold)
+	{
+		currentNumInRow++;
+		currentSum+=samples[numOfSamples-1];//summera
+	}
+	if(currentNumInRow!=0)
+	{
+		uint16_t ttt = (uint16_t)(currentSum/currentNumInRow);//return result
+		return ttt;//return result
+	}
+	else
+	{
+		return median(samples,numOfSamples);
+	}
 }
+#pragma GCC pop_options
+//end turn off optimization 
 
 uint16_t absThreshold(uint16_t a1, uint16_t a2)
 {
