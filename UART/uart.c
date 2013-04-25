f90899e3d58f8fd1c6366015c679e44e5e2ff610
Johan Källström <johka546@student.liu.se>
2013-04-25 20:12:27 +0200

    UART: ny handshakeide som inte låser hela alltet. ej testat.

--- UART/uart.c
+++ UART/uart.c
@@ -2,6 +2,8 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+
+
 /*
 Ställer in alla register samt hastighet för porten.
 */
@@ -14,34 +16,38 @@ void UART_init()
 	UBRR0 = baud;
 	//UBRR0H = (unsigned char)(baud>>8);
 	//UBRR0L = (unsigned char)baud;
-	/* Enable receiver and transmitter, and enable interrupts on receiving */
-	UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0);
+	/* Enable receiver and transmitter */
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
 	/* Set frame format: 8data, 1stop bit */
-	UCSR0C = (0<<USBS0)|(3<<UCSZ00);
-	
-	/// SÄTT UPP RX-BUFFER
+	UCSR0C = (0<<USBS0)|(3<<UCSZ00);//starta interupts efter handskakning.
+	handshaken = 0;
 
+	/// SÄTT UPP RX-BUFFER
 	cbInit(&_rxMessageBuffer, 100);
 }
+
 
 void UART_handshake(void)
 {
 	uint8_t svar=0;
-	while(svar!=2)
+	/* Wait for empty transmit buffer */
+	while (!( UCSR0A & (1<<UDRE0)));
+	/* Put data into buffer, sends the data */
+	UDR0 = 1;
+	
+	/* Wait for data to be received */
+	_delay_ms(10);//TODO trimma
+	if((UCSR0A & (1<<RXC0)))
 	{
-		/* Wait for empty transmit buffer */
-		while (!( UCSR0A & (1<<UDRE0)));
-		/* Put data into buffer, sends the data */
-		UDR0 = 1;
-		
-		/* Wait for data to be received */
-		_delay_ms(10);
-		if((UCSR0A & (1<<RXC0)))
-		{
-			/* Get and return received data from buffer */
-			svar=UDR0;
-		}
+		/* Get and return received data from buffer */
+		svar=UDR0;
 	}
+	if(svar==2)
+	{
+		handshaken=1;
+		UCSR0B |= (1 << RXCIE0);  //sets RXCIE0 in register UCSR0B to 1, to enable USART reception interrupt.
+	}
+
 	/* Wait for empty transmit buffer */
 	while (!( UCSR0A & (1<<UDRE0)));
 	/* Put data into buffer, sends the data */
@@ -51,21 +57,30 @@ void UART_handshake(void)
 /*
 Skickar meddelandet som ges av msg, byte för byte.
 */
-void UART_writeMessage(uint8_t* msg, uint8_t type, uint8_t size)
+uint8_t UART_writeMessage(uint8_t* msg, uint8_t type, uint8_t size)
 {
-	/* Wait for empty transmit buffer */
-	while (!( UCSR0A & (1<<UDRE0)));
-			
-	/* Put data into buffer, sends the data */
-	UDR0 = (type<<5)|size;//header data
-	
-	for(int i = 0; i < size; i++){
-		
+	if(handshaken)
+	{
 		/* Wait for empty transmit buffer */
 		while (!( UCSR0A & (1<<UDRE0)));
-		
+			
 		/* Put data into buffer, sends the data */
-		UDR0 = msg[i];
+		UDR0 = (type<<5)|size;//header data
+	
+		for(int i = 0; i < size; i++){
+		
+			/* Wait for empty transmit buffer */
+			while (!( UCSR0A & (1<<UDRE0)));
+		
+			/* Put data into buffer, sends the data */
+			UDR0 = msg[i];
+		}
+		return 1;
+	}
+	else
+	{
+		UART_handshake();
+		return 0;
 	}
 }
 
@@ -84,18 +99,16 @@ uint8_t UART_readMessage(uint8_t *msg, uint8_t *type, uint8_t *length)
 	{
 		return 0;
 	}
-	
+
 	uint8_t firstByte = cbRead(&_rxMessageBuffer);
 	*type=0b11100000&firstByte;
 	*type = *type>>5;
 	*length = firstByte&0x1F;
-	
+
 	for(int i = 0; i < *length; i++){
 		msg[i] = cbRead(&_rxMessageBuffer);
 	}
-	
-	
-	
+
 	return 1;
 }
 
