fdecdb7c138352e16c7a8b7d7106d5afc254ff0b
Johan Källström <johka546@student.liu.se>
2013-05-08 21:35:59 +0200

    Timeaware meanvalue filter with threshold

--- utils.c
+++ utils.c
@@ -34,6 +34,56 @@ uint16_t filterSampleArrayMeanPlusPlus(volatile uint16_t  *samples, uint8_t numO
 #pragma GCC pop_options
 //end turn off optimization 
 
+//turn off optimization 
+#pragma GCC push_options
+#pragma GCC optimize ("O0")
+uint16_t filterMeanTimeAware(volatile uint16_t  *samples, uint8_t numOfSamples, uint8_t threshold, uint8_t start)
+{
+	//räkna ut medelvärde
+	//ta bort för långa värden som uppkommer pga avbrott
+	//sluta läsa av arrayen om roboten står still
+	uint8_t currentNumInRow=0;
+	uint16_t currentSum=0;
+	uint8_t i = 0;
+	uint8_t n = start;
+	uint8_t spike = 0;
+	while(i < numOfSamples-1)
+	{
+		if(absThreshold(samples[n], samples[n-1]) < threshold)
+		{
+			currentNumInRow++;
+			currentSum+=samples[n];//summera
+		}
+		else
+		{
+			spike = 1;
+			break; //vi bildar inte medelvärde om det är utanför gränsen. t.ex står stilla.
+		}
+		n--;
+		i++;
+		if(n<0)
+		{
+			n=numOfSamples-1;
+		}
+	}
+	if((spike==0)&&(absThreshold(samples[start], samples[start+1]) < threshold))//testa sista om vi inte har fått en spike
+	{
+		currentNumInRow++;
+		currentSum+=samples[start+1];//summera
+	}
+	if(currentNumInRow!=0)
+	{
+		uint16_t ttt = (uint16_t)(currentSum/currentNumInRow);//return result
+		return ttt;//return result
+	}
+	else
+	{
+		return samples[start];//om vi har en -x^2 dvs derivatan runt den ser ish ut som en triangel.
+	}
+}
+#pragma GCC pop_options
+//end turn off optimization 
+
 uint16_t absThreshold(uint16_t a1, uint16_t a2)
 {
   if(a1 < a2)
--- utils.h
+++ utils.h
@@ -19,6 +19,7 @@
 #define BITVAL(x,y) (((x)>>(y)) & 1)
 
 uint16_t filterSampleArrayMeanPlusPlus(volatile uint16_t  *samples, uint8_t numOfSamples, uint8_t threshold);
+uint16_t filterMeanTimeAware(volatile uint16_t  *samples, uint8_t numOfSamples, uint8_t threshold, uint8_t start);
 uint16_t absThreshold(uint16_t a1, uint16_t a2);
 void insertionSort(volatile uint16_t *a, uint8_t array_size);
 uint16_t median(volatile uint16_t *a, uint8_t size);
